5ef22ecffa1c350696fabaf830e4fc19a9e5f6a1
tharushi manawage <tharushimanawage02@gmail.com>
2024-03-02 21:10:56 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,113 +1,26 @@
-// let date = new Date("2023-03-02");
+let random = Math.random()*10;
+let number = Math.round(random);
 
-// let date = new Date();
-// console.log(date.getDate());
-// console.log(date.getTime());
-// console.log(date.getFullYear());
+//let input = prompt("Enter a Number: ");
 
-// ----------------------------------------------------------
+function getNumber() {
+    const userInput = document.getElementById("input");
+    let integerNum = Number(userInput);
+    console.log(integerNum);
 
-// let num = Math.round(4.9);
-// let num = Math.round(4.49);
-// console.log(num);
+    let attempt = 3;
 
-// ----------------------------------------------------------
+    while (attempt != 0) {
+        attempt = attempt--;
 
-// let num = Math.random();
-// let num = Math.random()*11;
-// let num = Math.random().toFixed();
-// console.log(num);
+        if (integerNum > number) {
+            console.log(integerNum + " is grater than Generated Number..");
+        } else if (integerNum < number) {
+            console.log(integerNum + " is less than Generated Number..");
+        } else if (integerNum == number) {
+            console.log("Congratulations! You guessed the Number.");
+        } 
+    }
 
-// ----------------------------------------------------------
-
-// let customer = {
-//     name: "Saman",
-//     age: 22,
-//     address: "Panadura"
-// }
-
-// console.log(customer);
-
-// let jsonObj = JSON.stringify(customer);
-// console.log(jsonObj); // JSON conversion
-
-// console.log(JSON.parse(jsonObj)); // Javascript conversion
-
-// let jsObj = JSON.parse(jsonObj);
-// console.log(jsObj.name);
-
-// ----------------------------------------------------------
-
-// let fun = function() {
-//     console.log("Hello World!");
-// }
-// console.log(fun());
-
-// let fun = () => {
-//     console.log("Hello World!");
-// }
-// console.log(fun());
-
-// let fun = () => console.log("Hello World!");
-// console.log(fun());
-
-// let fun = (value) => console.log("Hello World!" + value);
-// console.log(fun(" :)"));
-
-// let fun = value => console.log("Hello World!" + value);
-// console.log(fun(" :)"));
-
-// let fun = value => {return "Hello World!" + value};
-// console.log(fun(" :)"));
-
-// ----------------------------------------------------------
-
-// fetch("https://restcountries.com/v3.1/all")
-// .then((response) => response.json())
-// .then((data) => console.log(data))
-
-// ----------------------------------------------------------
-
-// let countryList = document.getElementById("countryList");
-
-// fetch("https://restcountries.com/v3.1/all")
-// .then((response) => response.json())
-// .then((data) => {
-//     data.forEach(element => {
-//         let option = document.createElement("option");
-//         option.text = element.flag+element.name.common;
-//         countryList.appendChild(option);
-//         console.log(element.name.common);
-//     });
-// })
-
-// ----------------------------------------------------------
-
-let countryList = document.getElementById("countryList");
-let countryTable = document.getElementById("countryTable");
-
-let body =
-    `<tr>
-        <td>Name</td>
-        <td>Flag</td>
-    </tr>`;
-
-fetch("https://restcountries.com/v3.1/all")
-.then((response) => response.json())
-.then((data) => {
-    data.forEach(element => {
-        body +=
-            `<tr>
-                <td>${element.name.common}</td>
-                <td>${element.flag}</td>
-            </tr>`;
-
-        let option = document.createElement("option");
-        option.text = element.flag+element.name.common;
-        countryList.appendChild(option);
-        // console.log(element.name.common);
-    });
-    countryTable.innerHTML = body;
-})
-
-// ----------------------------------------------------------
+    console.log("Sorry, You Failed!");
+}
